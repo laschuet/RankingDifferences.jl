b0abfc65936a48d003b45b69080c791f23bdf312
laschuet <laschuet@users.noreply.github.com>
2020-06-10 01:03:46 +0200

    Add more fields

--- src/ranking.jl
+++ src/ranking.jl
@@ -11,13 +11,17 @@ abstract type AbstractRanking end
 Ranking.
 """
 struct Ranking{Ts<:Real} <: AbstractRanking
+    e::Set{Int}
+    q::Set{Int}
     S::Matrix{Ts}
-    q::Int
-    r::Vector{Int}
+    rl::Vector{Int}
+    rs::Vector{Ts}
 
-    function Ranking{Ts}(S::Matrix{Ts}, q::Integer, r::Vector{Int}) where {Ts<:Real}
-        return new(S, q, r)
+    function Ranking{Ts}(e::Set{Int}, q::Set{Int}, S::Matrix{Ts},
+                        rl::Vector{Int}, rs::Vector{Ts}) where {Ts<:Real}
+        return new(e, q, S, rl, rs)
     end
 end
-Ranking(S::Matrix{Ts}, q::Integer, r::Vector{Int}) where {Ts} =
-    Ranking{Ts}(S, q, r)
+Ranking(e::Set{Int}, q::Set{Int}, S::Matrix{Ts}, rl::Vector{Int},
+        rs::Vector{Ts}) where {Ts} =
+    Ranking{Ts}(e, q, S, rl, rs)
--- test/ranking.jl
+++ test/ranking.jl
@@ -1,10 +1,13 @@
 @testset "ranking" begin
+    e = Set([1, 2, 4])
+    q = Set([1])
+    S = [1 0 0.5; 0 1 0.5; 0.5 0.5 1]
+    rl = [3, 2]
+    rs = [3.0, 2]
+    r = Ranking(e, q, S, rl, rs)
+
     @testset "constructors" begin
-        S = [1 0 0.5; 0 1 0.5; 0.5 0.5 1]
-        q = 1
-        res = [3, 2]
-        r = Ranking(S, q, res)
         @test isa(r, Ranking)
-        @test r.S == S && r.q == q && r.r == res
+        @test r.e == e && r.q == q && r.S == S && r.rl == rl && r.rs == rs
     end
 end
